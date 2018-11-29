dbef858c2c9b83d2813fc66f62156fac71e28fe1
Brooke Lynn <brookelynnny2015@gmail.com>
2018-11-28 19:07:52 -0500

    IT WORKS

--- node-mysql-crud-app/app.js
+++ node-mysql-crud-app/app.js
@@ -6,9 +6,8 @@ const path = require('path');
 
 
 const bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json()); // parse form data client
-
 
 
 const {getHomePage} = require('./routes/index');
@@ -19,19 +18,19 @@ const port = 3000;
 
 //connecting to the database
 let db = mysql.createConnection({
-	host: 'localhost',
-	user: 'root',
-	password: 'Stormz!',
-	database: 'recipe'
+    host: 'localhost',
+    user: 'root',
+    password: 'Stormz!',
+    database: 'recipe'
 
 });
 
-db.connect(function(err) {
-	if (err) {
-		return console.error('error: ' + err.message);
-	}
-		console.log('connected to the MySQL server.');
-	});
+db.connect(function (err) {
+    if (err) {
+        return console.error('error: ' + err.message);
+    }
+    console.log('connected to the MySQL server.');
+});
 
 global.db = db;
 
@@ -56,106 +55,111 @@ app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
 //ROUTES
 //sign up page
-app.get('/signup', function(req, res) {
-	res.render('signup');
+app.get('/signup', function (req, res) {
+    res.render('signup');
 });
 
 //add recipe page
-app.get('/add', function(req, res) {
-	res.render('add-player');
+app.get('/add', function (req, res) {
+    res.render('add-player');
 });
 
 //edit recipe page
-app.get('/edit', function(req, res) {
-	res.render('edit-player');
+app.get('/edit', function (req, res) {
+    res.render('edit-player');
 });
 
 //dashboard page
-app.get('/dashboard', function(req, res) {
-	res.render('dashboard');
+app.get('/dashboard', function (req, res) {
+    res.render('dashboard');
 });
 
 //recipe page
-app.get('/recipe', function(req, res) {
-	res.render('recipe');
+app.get('/recipe', function (req, res) {
+    res.render('recipe');
 })
 
 
 //DATABASE
 //results page from entered search item
 app.get('/result', (req, res) => {
-	//obtain input value
-	let search = req.query.search;
+    //obtain input value
+    let search = req.query.search;
 //array with input
-let input = [search]
-console.log(search);
+    let input = [search]
+    console.log(search);
 
-	let query = "SELECT * FROM recipes WHERE 'name' LIKE '%'?'%' ";
-		db.query(query, input, (err, result) => {
-			if (err) {
-				console.log(err);
-				return res.status(500).send(err);
-			}
-			var recipes;
-			res.render('result', {
-				recipes:recipes
-			});
+    let query = "SELECT * FROM recipes WHERE name LIKE " + db.escape('%'+search+'%');
 
-		});
+    db.query(query, input, (err, result) => {
+        if (err) {
+            console.log(err);
+            return res.status(500).send(err);
+        }
+        var recipes = [];
+        for (var i = 0;i < result.length; i++) {
+            recipes.push({id: result[i].id, name: result[i].name, ingredients: result[i].ingrediants, instructions: result[i].instructions});
+        }
+        //var recipes = result;
+        res.render('result', {
+            recipes: recipes
+        });
+
+    });
 })
 
 //add recipe to database
-app.get('/testdb', (req,res) => {
-	let id = req.query.id;
-	let name = req.query.name;
-	let ingredients = req.query.ingredients;
-	let instructions = req.query.instructions;
-let input = [id, name, ingredients, instructions]
-console.log(id + ' ' + name + ' ' + ingredients);
-let query = "INSERT INTO recipes (id, name, ingrediants, instructions) VALUES (?, ?, ?, ?)";
-	db.query(query, input, (err, result) => {
-		if (err) {
-			console.log(err);
-			return res.status(500).send(err);
-		}
-		res.redirect('/');
-	});
+app.get('/testdb', (req, res) => {
+    let id = req.query.id;
+    let name = req.query.name;
+    let ingredients = req.query.ingredients;
+    let instructions = req.query.instructions;
+    let input = [id, name, ingredients, instructions]
+    console.log(id + ' ' + name + ' ' + ingredients);
+    let query = "INSERT INTO recipes (id, name, ingrediants, instructions) VALUES (?, ?, ?, ?)";
+    db.query(query, input, (err, result) => {
+        if (err) {
+            console.log(err);
+            return res.status(500).send(err);
+        }
+        res.redirect('/');
+    });
 })
 
 //add user to the DATABASE
-app.get('/testsignup', (req,res) => {
-	let firstname = req.query.firstname;
-	let lastname = req.query.lastname;
-	let email = req.query.email;
-	let skill = req.query.skill;
-	let username = req.query.username;
-	let password = req.query.password;
+app.get('/testsignup', (req, res) => {
+    let firstname = req.query.firstname;
+    let lastname = req.query.lastname;
+    let email = req.query.email;
+    let skill = req.query.skill;
+    let username = req.query.username;
+    let password = req.query.password;
 
-let input = [firstname, lastname, email, skill, username, password]
-console.log(firstname + ' ' + lastname);
-let query = "INSERT INTO users (fname, lname, email, skill, username, password) VALUES (?, ?, ?, ?, ?, ?)";
-	db.query(query, input, (err, result) => {
-		if (err) {
-			console.log(err);
-			return res.status(500).send(err);
-		}
-		res.redirect('/dashboard');
-	});
+    let input = [firstname, lastname, email, skill, username, password]
+    console.log(firstname + ' ' + lastname);
+    let query = "INSERT INTO users (fname, lname, email, skill, username, password) VALUES (?, ?, ?, ?, ?, ?)";
+    db.query(query, input, (err, result) => {
+        if (err) {
+            console.log(err);
+            return res.status(500).send(err);
+        }
+        res.redirect('/dashboard');
+    });
 })
 
 //add recipe to user's cookbook
-app.get('/star', (req,res) => {
-	let id = req.query.id;
+app.get('/star', (req, res) => {
+    let id = req.query.id;
 
-let input = [id]
-console.log(id);
-let query = "INSERT INTO cookbook (id) VALUES (?)";
-	db.query(query, input, (err, result) => {
-		if (err) {
-			console.log(err);
-			return res.status(500).send(err);
-		}
-		alert("Recipe added to your cookbook!");
-		res.redirect('/dashboard');
-	});
+    let input = [id]
+    console.log(id);
+    let query = "INSERT INTO cookbook (id) VALUES (?)";
+    db.query(query, input, (err, result) => {
+        if (err) {
+            console.log(err);
+            return res.status(500).send(err);
+        }
+        alert("Recipe added to your cookbook!");
+        res.redirect('/dashboard');
+    });
 })
